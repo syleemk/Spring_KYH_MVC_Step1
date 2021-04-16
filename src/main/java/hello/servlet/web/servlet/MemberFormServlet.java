753a1df3db876e04ca249b10b031a7b0afad196b
syleemk <syleemk@gmail.com>
2021-04-17 01:21:18 +0900

    [ADD] 회원관리 웹 애플리케이션 - 서블릿으로 동적 HTML 생성 응답

---
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,44 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        //요청 결과로 html을 뿌려줘야하기 때문에 response body의 content-type을 잡아줘야함
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        // 순수 서블릿으로만 html 렌더링하려면 이렇게 하드코딩해야함;; 
+        // 자바코드로 HTML 제공해야하기 때문에 쉽지 않은 작업
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                " <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                " username: <input type=\"text\" name=\"username\" />\n" +
+                " age: <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
+
